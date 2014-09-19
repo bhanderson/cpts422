655cdd573abd2974d52850bf3d7f878c9a4436a7
Michael McBreen <michaelmcbreen@comcast.net>
2014-09-18 22:34:39 -0700

    All tests included

--- HW2_Streams/IndexedNumberStream.cpp
+++ HW2_Streams/IndexedNumberStream.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
 #include "IndexedNumberStream.hpp"
 
+//this stream can always read
 bool IndexedNumberStream::CanRead()
 {
 	return true;
 }
 
+//this stream can never write
 bool IndexedNumberStream::CanWrite()
 {
 
 	return false;
 }
 
+//this stream can always seek
 bool IndexedNumberStream::CanSeek()
 {
 
 	return true;
 }
 
-
+//returns the total size set at creation
 i64 IndexedNumberStream::GetLength()
 {
 	return _size;
 }
 
+//returns currrent position
 i64 IndexedNumberStream::GetPosition()
 {
 	return _position;
 }
 
+//returns the position module 256
 unsigned int IndexedNumberStream::ProceduralGenerate()
 {
 	return GetPosition() % 256;
 }
 
+//reads into a buffer a set number of btyes
 int IndexedNumberStream::Read(void* buf, int byteCount)
 {
 	unsigned char * shit = (unsigned char *)buf;
@@ -43,6 +49,7 @@ int IndexedNumberStream::Read(void* buf, int byteCount)
 	}
 	for (int i = 0; i < byteCount; i++)
 	{
+		//uses the position to create the right number
 		shit[i] = ProceduralGenerate();
 		SetPosition(_position + 1);
 	}
@@ -50,6 +57,7 @@ int IndexedNumberStream::Read(void* buf, int byteCount)
 	return byteCount;
 }
 
+//checks if position is valid and sets it to it
 i64 IndexedNumberStream::SetPosition(i64 position)
 {
 	if (position >= 0 && position < _size)
@@ -59,11 +67,13 @@ i64 IndexedNumberStream::SetPosition(i64 position)
 	return _position;
 }
 
+//this stream never writes
 int IndexedNumberStream::Write(const void* buf, int byteCount)
 {
 	return -1;
 }
 
+//creates the stream
 IndexedNumberStream::IndexedNumberStream(int byteSize)
 {
 	_size = byteSize;
--- HW2_Streams/Source.cpp
+++ HW2_Streams/Source.cpp
@@ -105,6 +105,7 @@ char* BackwardsWrite(Stream* testStream){
 	return "BackwardsWrite Passed";
 }
 
+//reads a number of bytes and checks if they are the same as the expected values
 int ReadVerification(Stream* testStream, u8* values, int bytes)
 {
 	if (!testStream->CanRead())
@@ -124,6 +125,8 @@ int ReadVerification(Stream* testStream, u8* values, int bytes)
 	}
 	return SUCCESS;
 }
+
+//reads a stream from a postion in 3 chunks in order
 char* InOrderRead(Stream* testStream, u8 values1[], u8 values2[], u8 values3[], int bytes, int startPosition)
 {
 	int result;
@@ -145,6 +148,8 @@ char* InOrderRead(Stream* testStream, u8 values1[], u8 values2[], u8 values3[], 
 	}
 	return "InOrderRead Passed";
 }
+
+//reads a stream from a position in 3 chunks in reverse order
 char* ReverseRead(Stream* testStream, u8 values1[], u8 values2[], u8 values3[], int bytes, int startPosition)
 {
 	int result;
@@ -168,6 +173,8 @@ char* ReverseRead(Stream* testStream, u8 values1[], u8 values2[], u8 values3[], 
 	}
 	return "ReverseRead Passed";
 }
+
+//reads a stream from a positon in 3 chunks out of order
 char* RandomRead(Stream* testStream, u8 values1[], u8 values2[], u8 values3[], int bytes, int startPosition)
 {
 	int result;
@@ -192,26 +199,154 @@ char* RandomRead(Stream* testStream, u8 values1[], u8 values2[], u8 values3[], i
 	return "RandomRead Passed";
 }
 
+//reads the stream and checks it is correct
+char* ReadAllAtOnce(Stream* testStream, u8 data[], int bytes)
+{
+	if (!testStream->CanWrite())
+		return "ReadAllAtOnce Passed";
+	int result;
+	testStream->SetPosition(0);
+	result = ReadVerification(testStream, data, bytes);
+	if (result == SUCCESS)
+	{
+		return "ReadAllAtOnce Passed";
+	}
+	return "ReadAllAtOnce Failed";
+}
+
+//checks if a stream of 1024 bytes contains all 'a' in 4 chuncks
+char* ReadFixedCommon(Stream* testStream)
+{
+	if (!testStream->CanWrite())
+		return "FixedSizeReadCommon Passed";
+	int result;
+	testStream->SetPosition(0);
+	u8 common[1024] = { 'a' };
+	memset(common, 'a', 1024);
+	for (int i = 0; i < 4; i++)
+	{
+		result = ReadVerification(testStream, common, 256);
+		if (result == FAILURE)
+		{
+			"FixedSizeReadCommon Failed";
+		}
+	}
+	return "FixedSizeReadCommon Passed";
+}
+
+//checks to see if a stream is all b for 1801 bytes in 5 chunks and 1 extra
+char *ReadFixedUncommon(Stream* testStream){
+	if (!testStream->CanWrite())
+		return "FixedSizeReadUncommon Passed";
+	u8 uncommon[1801] = { 'b' };
+	int result;
+	testStream->SetPosition(0);
+	memset(uncommon, 'b', 1801);
+	for (int i = 0; i < 5; i++){
+		result = ReadVerification(testStream, uncommon, 360);
+		if (result == FAILURE)
+		{
+			"FixedSizeReadCommon Failed";
+		}
+	}
+	result = ReadVerification(testStream, uncommon, 1);
+	if (result != SUCCESS)
+		return "FixedSizeReadUncommon Failed";
+	return "FixedSizeReadUncommon Passed";
+
+}
+
+//reads the stream in backwards order with 2 chunks
+char* BackwardsRead(Stream* testStream){
+	if (!testStream->CanWrite())
+		return "BackwardsRead Passed";
+	int result;
+	testStream->SetPosition(512);
+	char a[512];
+	memset(a, 'a', 512);
+	result = ReadVerification(testStream, (u8*)a, 512);
+	if (result != SUCCESS)
+	{
+		return "BackwardsRead Failed";
+	}
+	testStream->SetPosition(0);
+	char b[512];
+	memset(b, 'b', 512);
+	result = ReadVerification(testStream, (u8*)b, 512);
+	if (result != SUCCESS)
+	{
+		return "BackwardsRead Failed";
+	}
+	return "BackwardsRead Passed";
+}
+
+//reads the stream in backwards order for every btye
+char* BackwardsReadbyBtye(Stream* testStream){
+	if (!testStream->CanWrite())
+		return "BackwardsReadByByte Passed";
+	int result;
+	testStream->SetPosition(1024);
+	char a[512];
+	memset(a, 'a', 512);
+	for (int i = 1024; i > 512; i--)
+	{
+		result = ReadVerification(testStream, (u8*)a, 1);
+		if (result != SUCCESS)
+		{
+			return "BackwardsReadByByte Failed";
+
+		}
+		testStream->SetPosition(i);
+	}
+	char b[512];
+	memset(b, 'b', 512);
+	for (int i = 512; i > 0; i--)
+	{
+		result = ReadVerification(testStream, (u8*)b, 1);
+		if (result != SUCCESS)
+		{
+			return "BackwardsReadByByte Failed";
+
+		}
+		testStream->SetPosition(i);
+	}
+	return "BackwardsReadByByte Passed";
+}
+
+//sets up and calls all unit tests for IndexedNumberStream
 void testIndexedStream()
 {
+	cout << "Testing IndexedNumbers Stream\n" << endl;
 	u8 temp1[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	u8 temp2[10] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
 	u8 temp3[10] = { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
-	IndexedNumberStream fuckIt(30);
-	Stream *Hate = &fuckIt;
-	cout << InOrderRead(Hate, temp1, temp2, temp3, 10, 0) << endl;
-	cout << ReverseRead(Hate, temp1, temp2, temp3, 10, 0) << endl;
-	cout << RandomRead(Hate, temp1, temp2, temp3, 10, 0) << endl;
-	cout << BackwardsWrite(Hate) << endl;
+	IndexedNumberStream dead(30);
+	Stream *beef = &dead;
+	cout << "Read Tests:\n------------------" << endl;
+	cout << InOrderRead(beef, temp1, temp2, temp3, 10, 0) << endl;
+	cout << ReverseRead(beef, temp1, temp2, temp3, 10, 0) << endl;
+	cout << RandomRead(beef, temp1, temp2, temp3, 10, 0) << endl;
+	cout << "\nWrite Tests:\n----------------" << endl;
+	cout << AllAtOnceWrite(beef, (u8 *)"ANYTHING", 10) << endl;
+	cout << ReadAllAtOnce(beef, (u8 *) "ANYTHING", 10) << endl;
+	cout << FixedSizeWriteCommon(beef) << endl;
+	cout << ReadFixedCommon(beef) << endl;
+	cout << FixedSizeWriteUncommon(beef) << endl;
+	cout << ReadFixedUncommon(beef) << endl;
+	cout << BackwardsWrite(beef) << endl;
+	cout << BackwardsRead(beef) << endl;
+	cout << BackwardsReadbyBtye(beef) << endl;
 }
 
+//sets up and call all unit tests for FileStream
 void testFileStream()
 {
-	ofstream os;
+	cout << "\nTesting File Stream\n" << endl;
+	ofstream os;// ("c:\\Users\\Bryce\\test.txt", ios::out | ios::in);
 	void *beef = "DEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEAD"
-				 "BEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFD"
-				 "EADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBE"
-				 "EFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF";
+		"BEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFD"
+		"EADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBE"
+		"EFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF";
 
 	os.open("read.txt");
 	os.write((char *)beef, 240);
@@ -220,40 +355,55 @@ void testFileStream()
 	Stream *a = &yolo;
 	cout << "Read Tests:\n------------------" << endl;
 	cout << InOrderRead(a, (u8 *)beef, (u8 *)beef, (u8 *)beef, 80, 0) << endl;
-	cout << ReverseRead(a, (u8 *) beef, (u8 *) beef, (u8 *) beef, 80, 0) << endl;
-	cout << RandomRead(a, (u8 *) beef, (u8 *) beef, (u8 *) beef, 80, 0) << endl;
+	cout << ReverseRead(a, (u8 *)beef, (u8 *)beef, (u8 *)beef, 80, 0) << endl;
+	cout << RandomRead(a, (u8 *)beef, (u8 *)beef, (u8 *)beef, 80, 0) << endl;
 	FileStream write("write.txt", ios::out);
 	Stream *b = &write;
-	cout << "Write Tests:\n----------------" << endl;
-	cout << AllAtOnceWrite(b,(u8 *) beef,240) << endl;
+	cout << "\nWrite Tests:\n----------------" << endl;
+	cout << AllAtOnceWrite(b, (u8 *)beef, 240) << endl;
+	cout << ReadAllAtOnce(b, (u8 *)beef, 240) << endl;
 	cout << FixedSizeWriteCommon(b) << endl;
+	cout << ReadFixedCommon(b) << endl;
 	cout << FixedSizeWriteUncommon(b) << endl;
+	cout << ReadFixedUncommon(b)<<endl;
 	cout << BackwardsWrite(b) << endl;
+	cout << BackwardsRead(b) <<endl;
+	cout << BackwardsReadbyBtye(b) << endl;
+
 }
 
+//sets up and call all unit tests for MemoryStream()
 void testMemoryStream()
 {
+	cout << "\nTest MemoryStream\n" << endl;
 	void *beef = "DEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEAD"
 		"BEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFD"
 		"EADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBE"
 		"EFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF";
-	
+
 	MemoryStream write(16);
+	MemoryStream read(16);
 	Stream *a = &write;
 	cout << "Write Tests:\n----------------" << endl;
 	cout << AllAtOnceWrite(a, (u8 *)beef, 240) << endl;
+	cout << ReadAllAtOnce(a, (u8 *)beef, 240) << endl;
 	cout << FixedSizeWriteCommon(a) << endl;
+	cout << ReadFixedCommon(a) << endl;
 	cout << FixedSizeWriteUncommon(a) << endl;
-	cout << "Read Tests:\n------------------" << endl;
+	cout << ReadFixedUncommon(a) << endl;
+	cout << BackwardsWrite(a) << endl;
+	cout << BackwardsRead(a) << endl;
+	cout << BackwardsReadbyBtye(a) << endl;
+	a = &read;
+	cout << "\nRead Tests:\n------------------" << endl;
 	cout << InOrderRead(a, (u8 *)beef, (u8 *)beef, (u8 *)beef, 80, 0) << endl;
 	cout << ReverseRead(a, (u8 *)beef, (u8 *)beef, (u8 *)beef, 80, 0) << endl;
 	cout << RandomRead(a, (u8 *)beef, (u8 *)beef, (u8 *)beef, 80, 0) << endl;
-	cout << BackwardsWrite(a) << endl;
 }
 
+//calls all stream test functions
 int main(int argc, const char* argv[])
 {
-
 	testIndexedStream();
 	testFileStream();
 	testMemoryStream();
